e00c61156953ade61f88fd0cd4f1eb8db00783a8
Naomi Giselle Park <ngpark@caddy15.stanford.edu>
2026-02-27 11:20:22 -0800

    adding LW

---
+++ fv/synthlc/i_LW_out/xHBPerfG_dfg_v3_div/WHB_360_rundir/WHB_360_jgsession_26-02-27-10_50_57/sessionLogs/session_0/jg_session_0.cmd
@@ -0,0 +1,108 @@
+# ----------------------------------------
+# Jasper Version Info
+# tool      : Jasper 2023.12
+# platform  : Linux 4.18.0-553.89.1.el8_10.x86_64
+# version   : 2023.12p001 64 bits
+# build date: 2024.01.23 16:09:24 UTC
+# ----------------------------------------
+# started   : 2026-02-27 10:50:58 PST
+# hostname  : caddy15.stanford.edu.(none)
+# pid       : 777107
+# arguments : '-label' 'session_0' '-console' '//127.0.0.1:36769' '-nowindow' '-style' 'windows' '-data' 'AAAB+HictZDNSsNAFIW/KIq4EJc+g1BbI7UuuhBBdFGpP6DLIK2tKSEpTaw/G30IH9A3iV9Tu8gDOMOcO/fcc+4dJgC6n2VZUq31D2GXHlfcci72uTfCHm1CTmhxyoBHhiTE3lpLX/CzjHQD6muRr9WZm69ahI2VeSXZ9OzTcE7izngl4oWUXJy6M2YUPPmKyBfeqd5RncpHjNXEMlsyI7Vz7yMOaOp+V1PwbM+Beay6x4OY6Slk3rjkjGs7z/RcWBkax8Y5R1UWe2vqWegiuWN/ILJ7YcfESdtOnerOmJh9//PciZpcV+6O7ZbKhdYaVkMxpCMeVtr2H3aqH/4FZipHOg==' '-proj' '/home/users/ngpark/SynthLC/fv/synthlc/i_LW_out/xHBPerfG_dfg_v3_div/WHB_360_jgsession_26-02-27-10_50_57/sessionLogs/session_0' '-init' '-hidden' '/home/users/ngpark/SynthLC/fv/synthlc/i_LW_out/xHBPerfG_dfg_v3_div/WHB_360_jgsession_26-02-27-10_50_57/.tmp/.initCmds.tcl' './synthlc/i_LW_out/xHBPerfG_dfg_v3_div/WHB_360_.tcl'
+# Run JG with a TCl file: jg jg_test.tcl
+set assert_report_incompletes 1
+set RTL_DIR /home/users/cheriek/Documents/Formal_Method/final_project/ibex
+set SRCDIR /home/users/cheriek/Documents/Formal_Method/final_project/ibex/rtl
+
+############
+set FPV 1
+set REACH 0
+set CUSTOMTCL 0
+############
+exec ./synthlc/i_LW_out/xHBPerfG_dfg_v3_div/WHB_360_update_file_.sh
+# Analyze RTL files
+analyze -sv09 -f ./synthlc/i_LW_out/xHBPerfG_dfg_v3_div/WHB_360_hdls.f -y $SRCDIR +incdir+$SRCDIR
+
+if {$REACH == 1} {
+    puts "test bboxing mfpt" 
+    #elaborate -bbox_m  {miss_prediction_fix_table} -bbox_m {reorderbuf}
+    #elaborate
+    source reach_collect.tcl
+}
+if {$FPV == 1} {
+    puts "fpv" 
+    # Elaborates
+    #elaborate -bbox_m {wt_cache_subsystem} -bbox_m {ibex_if_stage}
+    #puts "multiplier no-bbox"
+    elaborate -top ibex_fv -bbox_m {ibex_if_stage} -bbox_m {prim_lfsr}
+    #elaborate -bbox_m {ibex_if_stage}
+    #stopat -env {issue_stage_i.i_scoreboard.mem_n[0].sbe.is_compressed}  
+    #elaborate
+
+
+    # Initialization
+    # Clock specification
+    clock clk_i
+    # -both_edges: ridecore 
+    reset !rst_ni
+    set_proofgrid_per_engine_max_jobs 10
+    set_proofgrid_max_jobs 30
+
+
+    #SOURCE_TCL
+    # assume -enable {.*ASSUME_W_R} -regexp
+    # assume -disable {.*ASSUME_R_W} -regexp
+
+    task -create mytask -copy_assumes -copy {.*HB_360.*} -copy {.*WHB_360.*} -copy {.*WHB_CONCUR_360.*}   -regexp
+    task -set mytask
+
+    if { $CUSTOMTCL == 1 } {
+        puts "=========CUSTOMTCL========="
+
+        #CUSTOMTCL
+        
+        puts "=========EXIT CUSTOMTCL========="
+        exit
+        
+    } 
+    
+    puts "=============================================================="
+    puts "CHECK ASSUMPTION...."
+    puts "=============================================================="
+    set CA 0
+    #ASSUMPTION
+    #
+    set_prove_time_limit 15m
+    #SETPROVETIME
+    set_prove_per_property_time_limit 5m 
+
+    set ls [get_property_list -task mytask -include {type {assert cover} }]
+    if { [llength $ls] > 10 } { 
+        set_prove_time_limit 25m 
+        puts "PROVEN TIME 25min" }  
+
+    if { $CA == 1 } { 
+        #set_prove_time_limit 10m
+        set CONFLICT [check_assumptions -task mytask -conflict]
+        puts "=============================================================="
+        puts "CHECK ASSUMPTION CONFLICT result? $CONFLICT"
+        puts "=============================================================="
+    }  else {
+        puts "AUTOPROVE:" 
+        #set_prove_time_limit 1h
+        #set_prove_per_property_time_limit 1h
+        puts "=================================================="
+        puts " PROVE TIME LIMIT"
+        puts [get_prove_time_limit]     
+        #puts [get_prove_per_property_time_limit]
+        puts "=================================================="
+        set_engine_mode {K C Tri I N AD AM Hp B}
+
+        prove -task mytask
+        #prove -all
+    }
+    
+    puts "END"
+    report -task mytask -csv -results -file "./synthlc/i_LW_out/xHBPerfG_dfg_v3_div/WHB_360.csv" -force
+    exit
+}
---
+++ fv/synthlc/i_LW_out/xPerfLocSubsetDiv/coverset_2_rundir/coverset_2_jgsession_26-02-27-10_50_19/sessionLogs/session_0/jg_session_0.cmd
@@ -0,0 +1,108 @@
+# ----------------------------------------
+# Jasper Version Info
+# tool      : Jasper 2023.12
+# platform  : Linux 4.18.0-553.89.1.el8_10.x86_64
+# version   : 2023.12p001 64 bits
+# build date: 2024.01.23 16:09:24 UTC
+# ----------------------------------------
+# started   : 2026-02-27 10:50:20 PST
+# hostname  : caddy15.stanford.edu.(none)
+# pid       : 774013
+# arguments : '-label' 'session_0' '-console' '//127.0.0.1:41209' '-nowindow' '-style' 'windows' '-data' 'AAAB/HictZFJTsNAEEWfQSDEArHkDEghCRKTlAUL2AUxSbBsgQngyLKjxDHDBk7B+biJebaVRQ5Al7qq69evobsjYPBVVRXNWv1UbTPkghvO1ZfcaWGHA/Y5pscpMQ88kZJ46rV50W9rGUQsr9pfWUauv5csrC2SF5R19y4d+6RKzhuBORkz9UTJmVIwcorghLeyt2Rn4oEXOYnIhsiz3NLzK3t0zf6QU+ilTt6VFbzpvTo3pxB558qqU/OGYrGV5zyaNzJ6Jr+UExspG1aLB98lWL8wktpr074TozljvZ9/7zz2xrU/UxIZWYMfOkVP23Efqft6wT+sdZ+T5pX/AC+BSto=' '-proj' '/home/users/ngpark/SynthLC/fv/synthlc/i_LW_out/xPerfLocSubsetDiv/coverset_2_jgsession_26-02-27-10_50_19/sessionLogs/session_0' '-init' '-hidden' '/home/users/ngpark/SynthLC/fv/synthlc/i_LW_out/xPerfLocSubsetDiv/coverset_2_jgsession_26-02-27-10_50_19/.tmp/.initCmds.tcl' './synthlc/i_LW_out/xPerfLocSubsetDiv/coverset_2_.tcl'
+# Run JG with a TCl file: jg jg_test.tcl
+set assert_report_incompletes 1
+set RTL_DIR /home/users/cheriek/Documents/Formal_Method/final_project/ibex
+set SRCDIR /home/users/cheriek/Documents/Formal_Method/final_project/ibex/rtl
+
+############
+set FPV 1
+set REACH 0
+set CUSTOMTCL 0
+############
+exec ./synthlc/i_LW_out/xPerfLocSubsetDiv/coverset_2_update_file_.sh
+# Analyze RTL files
+analyze -sv09 -f ./synthlc/i_LW_out/xPerfLocSubsetDiv/coverset_2_hdls.f -y $SRCDIR +incdir+$SRCDIR
+
+if {$REACH == 1} {
+    puts "test bboxing mfpt" 
+    #elaborate -bbox_m  {miss_prediction_fix_table} -bbox_m {reorderbuf}
+    #elaborate
+    source reach_collect.tcl
+}
+if {$FPV == 1} {
+    puts "fpv" 
+    # Elaborates
+    #elaborate -bbox_m {wt_cache_subsystem} -bbox_m {ibex_if_stage}
+    #puts "multiplier no-bbox"
+    elaborate -top ibex_fv -bbox_m {ibex_if_stage} -bbox_m {prim_lfsr}
+    #elaborate -bbox_m {ibex_if_stage}
+    #stopat -env {issue_stage_i.i_scoreboard.mem_n[0].sbe.is_compressed}  
+    #elaborate
+
+
+    # Initialization
+    # Clock specification
+    clock clk_i
+    # -both_edges: ridecore 
+    reset !rst_ni
+    set_proofgrid_per_engine_max_jobs 10
+    set_proofgrid_max_jobs 30
+
+
+    #SOURCE_TCL
+    # assume -enable {.*ASSUME_W_R} -regexp
+    # assume -disable {.*ASSUME_R_W} -regexp
+
+    task -create mytask -copy_assumes  -copy {.*C_2_N.*}  -regexp
+    task -set mytask
+
+    if { $CUSTOMTCL == 1 } {
+        puts "=========CUSTOMTCL========="
+
+        #CUSTOMTCL
+        
+        puts "=========EXIT CUSTOMTCL========="
+        exit
+        
+    } 
+    
+    puts "=============================================================="
+    puts "CHECK ASSUMPTION...."
+    puts "=============================================================="
+    set CA 0
+    #ASSUMPTION
+    #
+    set_prove_time_limit 15m
+    #SETPROVETIME
+    set_prove_per_property_time_limit 5m 
+
+    set ls [get_property_list -task mytask -include {type {assert cover} }]
+    if { [llength $ls] > 10 } { 
+        set_prove_time_limit 25m 
+        puts "PROVEN TIME 25min" }  
+
+    if { $CA == 1 } { 
+        #set_prove_time_limit 10m
+        set CONFLICT [check_assumptions -task mytask -conflict]
+        puts "=============================================================="
+        puts "CHECK ASSUMPTION CONFLICT result? $CONFLICT"
+        puts "=============================================================="
+    }  else {
+        puts "AUTOPROVE:" 
+        #set_prove_time_limit 1h
+        #set_prove_per_property_time_limit 1h
+        puts "=================================================="
+        puts " PROVE TIME LIMIT"
+        puts [get_prove_time_limit]     
+        #puts [get_prove_per_property_time_limit]
+        puts "=================================================="
+        set_engine_mode {K C Tri I N AD AM Hp B}
+
+        prove -task mytask
+        #prove -all
+    }
+    
+    puts "END"
+    report -task mytask -csv -results -file "./synthlc/i_LW_out/xPerfLocSubsetDiv/coverset_2.csv" -force
+    exit
+}
