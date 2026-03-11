a9ec267b99f9d1414fe2c93bf75e3b2ae9620492
naomipark3 <77819519+naomipark3@users.noreply.github.com>
2026-03-10 22:31:29 -0700

    Delete fv/synthlc/i_BEQ_out_updated_header directory

--- fv/synthlc/i_BEQ_out_updated_header/xPairwiseDepDiv/nunv_anytwo_14_rundir/nunv_anytwo_14_update_file_.sh
+++
@@ -1,8 +0,0 @@
-
-if [ -f "./synthlc/i_BEQ_out/xPairwiseDepDiv/out/nunv_anytwo_14.sv" ] && [ -f "./ibex_fv/ibex_topsim.sv" ]; then
-    { head -n -1 "./ibex_fv/ibex_topsim.sv"; cat "./ibex_fv/ibex_header_fv.sv"; cat "./synthlc/i_BEQ_out/xPairwiseDepDiv/out/nunv_anytwo_14.sv" ; echo "" ; tail -n 1 "./ibex_fv/ibex_topsim.sv"; } > "./synthlc/i_BEQ_out/xPairwiseDepDiv/nunv_anytwo_14_top.sv" #"/_top.sv"
-else 
-    echo "[RUN_JG] no property at ./synthlc/i_BEQ_out/xPairwiseDepDiv/out/nunv_anytwo_14.sv is found or no ./ibex_fv/ibex_topsim.sv"
-    exit 0
-fi
-
