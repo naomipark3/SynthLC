f71474857d7b6f1cc0b58a4d214105770090a5bc
naomipark3 <77819519+naomipark3@users.noreply.github.com>
2026-03-10 17:15:46 -0700

    Delete fv/synthlc/i_MUL_out_last_shot directory

--- fv/synthlc/i_MUL_out_last_shot/xCollectReEvalLeaveOrder/com_2_0_rundir/com_2_0_update_file_.sh
+++
@@ -1,8 +0,0 @@
-
-if [ -f "./synthlc/i_MUL_out/xCollectReEvalLeaveOrder/out_complete_2_setcover/com_2_0.sv" ] && [ -f "./ibex_fv/ibex_topsim.sv" ]; then
-    { head -n -1 "./ibex_fv/ibex_topsim.sv"; cat "./ibex_fv/ibex_header_fv.sv"; cat "./synthlc/i_MUL_out/xCollectReEvalLeaveOrder/out_complete_2_setcover/com_2_0.sv" ; echo "" ; tail -n 1 "./ibex_fv/ibex_topsim.sv"; } > "./synthlc/i_MUL_out/xCollectReEvalLeaveOrder/com_2_0_top.sv" #"/_top.sv"
-else 
-    echo "[RUN_JG] no property at ./synthlc/i_MUL_out/xCollectReEvalLeaveOrder/out_complete_2_setcover/com_2_0.sv is found or no ./ibex_fv/ibex_topsim.sv"
-    exit 0
-fi
-
