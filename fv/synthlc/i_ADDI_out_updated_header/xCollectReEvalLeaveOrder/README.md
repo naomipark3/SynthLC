7dfa4459968fa0babf069cc2c3bdcd62ddb53122
naomipark3 <77819519+naomipark3@users.noreply.github.com>
2026-03-10 22:31:08 -0700

    Delete fv/synthlc/i_ADDI_out_updated_header directory

--- fv/synthlc/i_ADDI_out_updated_header/xCollectReEvalLeaveOrder/README.md
+++
@@ -1,3 +0,0 @@
-Given xCollectReEval result, extract all combination of entering order that is
-possible, check if its realizable, and explore for each combintion of entering
-order, the exit order. 
--- fv/synthlc/i_ADDI_out_updated_header/xPairwiseDepDiv/nunv_anytwo_1_rundir/nunv_anytwo_1_update_file_.sh
+++
@@ -1,8 +0,0 @@
-
-if [ -f "./synthlc/i_ADDI_out/xPairwiseDepDiv/out/nunv_anytwo_1.sv" ] && [ -f "./ibex_fv/ibex_topsim.sv" ]; then
-    { head -n -1 "./ibex_fv/ibex_topsim.sv"; cat "./ibex_fv/ibex_header_fv.sv"; cat "./synthlc/i_ADDI_out/xPairwiseDepDiv/out/nunv_anytwo_1.sv" ; echo "" ; tail -n 1 "./ibex_fv/ibex_topsim.sv"; } > "./synthlc/i_ADDI_out/xPairwiseDepDiv/nunv_anytwo_1_top.sv" #"/_top.sv"
-else 
-    echo "[RUN_JG] no property at ./synthlc/i_ADDI_out/xPairwiseDepDiv/out/nunv_anytwo_1.sv is found or no ./ibex_fv/ibex_topsim.sv"
-    exit 0
-fi
-
